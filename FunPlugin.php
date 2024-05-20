5edd836e84198e4500a9dd96090e00b38c908d9c
farisgarib <farisgarib96@gmail.com>
2024-05-20 22:07:21 +0200

    Update FunPlugin.php

--- FunPlugin.php
+++ FunPlugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Plugin Name: Fun Plugin
-* Plugin URI: 
+* Plugin URI: https://github.com/iamsajidjaved/Auto-update-WordPress-plugin-or-Theme-from-Github
 * Description: This is a Fun Plugin.
 * Version: 2.0
 * Author: Sajid Javed
@@ -10,7 +10,7 @@
 
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'',
+	'https://github.com/farisgarib/dexpress-woo/',
 	__FILE__,
 	'FunPlugin'
 );
@@ -19,4 +19,4 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 $myUpdateChecker->setBranch('main');
 
 //Optional: If you're using a private repository, specify the access token like this:
-$myUpdateChecker->setAuthentication('');
+$myUpdateChecker->setAuthentication('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');
